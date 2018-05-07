7d962da87b87fae3a74ca13358f92585683fbfdf
Nine_Bro <shinjisrg@gmail.com>
2018-05-07 23:53:06 +0800

    Fixed capitalize typo of 'integer' in migrate file of Books table, and had all migration successed.

--- db/migrate/20180507153724_create_books.rb
+++ db/migrate/20180507153724_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.Integer :price
+      t.integer :price
       t.references :author, foreign_key: true
 
       t.timestamps
